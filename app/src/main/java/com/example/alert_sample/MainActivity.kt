f6bdc8ce578af02c8e6d9c3713de4dd20891bb95
YAMAZAKI RIKU <yamazaki.riku@chrono.co.jp>
2022-10-03 17:41:14 +0900

    １回のみ表示 SharedPreferencesを使用 falseであればモーダル表示、true書き込み trueであれば無動作

--- app/src/main/java/com/example/alert_sample/MainActivity.kt
+++ app/src/main/java/com/example/alert_sample/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.alert_sample
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -14,7 +16,15 @@ class MainActivity : AppCompatActivity() {
 
 //ボタン無し　アプリ起動時に自動で表示されるフラグメント（fragment_news.xmlにてレイアウト）
         val dialog = NewsFragment()
-        dialog.show(supportFragmentManager, "")
+        val data: SharedPreferences = getSharedPreferences("NewsShowed", Context.MODE_PRIVATE)
+        val dataBoolean = data.getBoolean("NewsShowedBoolean", false)
+
+        if (!dataBoolean) {
+            dialog.show(supportFragmentManager, "")
+            val editor = data.edit()
+            editor.putBoolean("NewsShowedBoolean", true)
+            editor.apply()
+        }
 
 //ボタン１つめ　上のボタンを押すと表示される純粋なダイアログサンプル
         val btnAlertDialog: Button = findViewById(R.id.btnAlert)
