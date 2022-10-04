e3a8bde1fda0010ed8902df8f50f72d8d89c4b42
YAMAZAKI RIKU <yamazaki.riku@chrono.co.jp>
2022-10-04 18:59:58 +0900

    Log.d追加 画面回転でonCreateしないよう、 Manifestにandroid:configChanges="orientation|screenSize"を追記

--- app/src/main/java/com/example/alert_sample/MainActivity.kt
+++ app/src/main/java/com/example/alert_sample/MainActivity.kt
@@ -5,36 +5,52 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("LOG_01A","onCreate_MainActivity")
         super.onCreate(savedInstanceState)
+        Log.d("LOG_02A","super.onCreate_MainActivity")
         setContentView(R.layout.activity_main)
+        Log.d("LOG_03A","setContentView_MainActivity")
 
 //ボタン無し　アプリ起動時に自動で表示されるフラグメント（fragment_news.xmlにてレイアウト）
         val dialog = NewsFragment()
-        val data: SharedPreferences = getSharedPreferences("NewsShowed", Context.MODE_PRIVATE)
-        val dataBoolean = data.getBoolean("NewsShowedBoolean", false)
+        Log.d("LOG_04A","dialog=NewsFragment()_MainActivity")
 
+        val data: SharedPreferences = getSharedPreferences("NewsShowed", Context.MODE_PRIVATE)
+
+        val dataBoolean = data.getBoolean("NewsShowedBoolean", false)
+        Log.d("LOG_05A","getBoolean_MainActivity")
+
+        Log.d("LOG_06A","if_MainActivity")
         if (!dataBoolean) {
+            Log.d("LOG_07A","(if==true)_MainActivity")
             dialog.show(supportFragmentManager, "")
             val editor = data.edit()
             editor.putBoolean("NewsShowedBoolean", true)
             editor.apply()
+        }else {
+            Log.d("LOG_08A", "(if==false)_MainActivity")
         }
 
 //ボタン１つめ　上のボタンを押すと表示される純粋なダイアログサンプル
         val btnAlertDialog: Button = findViewById(R.id.btnAlert)
+        Log.d("LOG_09A","val_btnFirst_MainActivity")
+
         btnAlertDialog.setOnClickListener {
+            Log.d("LOG_10A","btnAlertDialog_MainActivity")
             AlertDialog.Builder(this)
                 .setTitle("タイトル")
                 .setMessage("内容")
                 .setNegativeButton("閉じる", null)
                 .setCancelable(false)
                 .show()
+            Log.d("LOG_11A","dialog.show_btnFirst_MainActivity")
         }
 
 //ボタン２つめ　下のボタンを押すと表示されるダイアログ（theme.xmlにてデザインをカスタマイズ(上記と同一部品)）
@@ -44,12 +60,14 @@ class MainActivity : AppCompatActivity() {
                 .setCancelable(false)
                 .setNegativeButton(" ", null)
                 .show()
+            Log.d("LOG_12A","dialog.show_btnSecond_MainActivity")
         }
 
 //ボタン３つめ　起動時の自動表示フラグメントを呼び出す
         val btnThird: Button = findViewById(R.id.btnThird)
         btnThird.setOnClickListener {
             dialog.show(supportFragmentManager, "")
+            Log.d("LOG_13A","dialog.show_btnThird_MainActivity")
         }
     }
 }
--- app/src/main/java/com/example/alert_sample/NewsFragment.kt
+++ app/src/main/java/com/example/alert_sample/NewsFragment.kt
@@ -4,6 +4,7 @@ import android.app.Dialog
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import android.widget.ImageButton
 import androidx.fragment.app.DialogFragment
@@ -11,22 +12,29 @@ import androidx.fragment.app.DialogFragment
 class NewsFragment : DialogFragment(){
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        Log.d("LOG_01F","onCreateDialog_NewsFragment")
         val dialog = Dialog(this.requireActivity())
+        Log.d("LOG_02F","val_dialog_NewsFragment")
 
         dialog.window!!.requestFeature(Window.FEATURE_NO_TITLE)
-        dialog.window!!.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN)
+//        dialog.window!!.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN)
         dialog.setContentView(R.layout.fragment_news)
         dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         dialog.setCanceledOnTouchOutside(false)
+        Log.d("LOG_03F","dialog_setContentView_NewsFragment")
 
         dialog.findViewById<ImageButton>(R.id.imgBtnNews)
             .setOnClickListener{
                 dismiss()
+                Log.d("LOG_04F","dismiss_btnImage_NewsFragment")
             }
         dialog.findViewById<ImageButton>(R.id.imgBtnClose)
             .setOnClickListener{
                 dismiss()
+                Log.d("LOG_04'F","dismiss_btnX_NewsFragment")
             }
         return dialog
+        Log.d("LOG_05F","return_dialog_NewsFragment")
+
     }
 }
