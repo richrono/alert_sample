e0776b9425a89ce6c564a78d5c629e5c571ab3c9
YAMAZAKI RIKU <yamazaki.riku@chrono.co.jp>
2022-10-13 16:28:59 +0900

    GitPush用にLog.g削除（NewsFragment）

--- app/src/main/java/com/example/alert_sample/NewsFragment.kt
+++ app/src/main/java/com/example/alert_sample/NewsFragment.kt
@@ -10,31 +10,22 @@ import android.widget.ImageButton
 import androidx.fragment.app.DialogFragment
 
 class NewsFragment : DialogFragment(){
-
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        Log.d("LOG_01F","onCreateDialog_NewsFragment")
         val dialog = Dialog(this.requireActivity())
-        Log.d("LOG_02F","val_dialog_NewsFragment")
 
         dialog.window!!.requestFeature(Window.FEATURE_NO_TITLE)
-//        dialog.window!!.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN)
         dialog.setContentView(R.layout.fragment_news)
         dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         dialog.setCanceledOnTouchOutside(false)
-        Log.d("LOG_03F","dialog_setContentView_NewsFragment")
 
         dialog.findViewById<ImageButton>(R.id.imgBtnNews)
             .setOnClickListener{
                 dismiss()
-                Log.d("LOG_04F","dismiss_btnImage_NewsFragment")
             }
         dialog.findViewById<ImageButton>(R.id.imgBtnClose)
             .setOnClickListener{
                 dismiss()
-                Log.d("LOG_04'F","dismiss_btnX_NewsFragment")
             }
         return dialog
-        Log.d("LOG_05F","return_dialog_NewsFragment")
-
     }
 }
